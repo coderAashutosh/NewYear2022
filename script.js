9c59b81ddc0f52db5fb332dc9a3ba6a800b1c460
Aashutosh Bansal <75746087+coderAashutosh@users.noreply.github.com>
2021-11-23 20:37:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let days_show = document.querySelector('#days');
+let hours_show = document.querySelector('#hours');
+let minutes_show = document.querySelector('#minutes');
+let seconds_show = document.querySelector('#seconds');
+let countdown = document.querySelector('#countdown');
+let title = document.querySelector('#title');
+
+//set time
+let new_year_date = new Date('Jan 1, 2022 00:00:00').getTime();
+
+//create a function (update)
+let timer = setInterval(update, 1000);
+
+//This function will updated every 1 second
+function update() {
+
+    //present time
+    let present_time = new Date().getTime();
+
+    //remaining time
+    let remaining_time = new_year_date - present_time;
+
+    //count remaining days, hours, minutes & seconds
+    let days = Math.floor(remaining_time / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(remaining_time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(remaining_time % (1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(remaining_time % (1000 * 60) / (1000));
+
+    //checking odd number
+    if (seconds % 2 !== 0) {
+        seconds_show.style.color = "#fff";
+    } else {
+        seconds_show.style.color = "#ac5af8";
+    }
+
+    //show time
+    days_show.innerHTML = days;
+    hours_show.innerHTML = hours;
+    minutes_show.innerHTML = minutes;
+    seconds_show.innerHTML = seconds;
+
+    //function will run when time is done
+    if (remaining_time <= 0) {
+        clearInterval(timer);
+        countdown.style.display = "none";
+        title.innerHTML = "May this year bring peace and happiness in your life.😊";
+    }
+
+}
